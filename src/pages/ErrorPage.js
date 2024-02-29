e212fb95bd419a441a12464a7b6234c1a069d54f
Sung-Jin Ahn <sahn0307@gmail.com>
2024-02-29 09:34:00 -0700

    pass tests

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,17 @@
+import NavBar from '../components/NavBar'
+import { useRouteError } from 'react-router-dom'
+
+function ErrorPage() {
+
+    const error = useRouteError();
+    console.error(error);
+
+    return (
+        <>
+            <NavBar />
+            <h1>Oops! Looks like something went wrong.</h1>
+        </>
+    )
+}
+
+export default ErrorPage
